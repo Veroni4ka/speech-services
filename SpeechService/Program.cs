bab9eb493fa0fe0e3bcf564c2020ea7031c5e580
Veronika Kolesnikova <VKolesnikova@rightpoint.com>
2019-03-19 16:41:09 -0400

    Add continuous recognition method and speech synthesizing

--- SpeechService/Program.cs
+++ SpeechService/Program.cs
@@ -1,19 +1,21 @@
+using System.Configuration;
 using System;
 using System.Globalization;
 using System.Linq;
+using System.Speech.Synthesis;
 using System.Threading.Tasks;
 using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Intent;
-using Newtonsoft.Json;
+using Microsoft.CognitiveServices.Speech.Translation;
 using Newtonsoft.Json.Linq;
 
 namespace SpeechService
 {
     class Program
     {
-        public static async Task RecognizeSpeechAsync()
+        public static async Task RecognizeOnceSpeechAsync(SpeechTranslationConfig config)
         {
-            var config = SpeechTranslationConfig.FromSubscription("421bec868d334e249897e9b2432af7b9", "eastus2");
+            
             var allCultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
 
             // Creates a speech recognizer.
@@ -21,7 +23,7 @@ namespace SpeechService
             {
                 Console.WriteLine("Say something...");
 
-                var model = LanguageUnderstandingModel.FromAppId("65332712-8c79-41b5-86bb-841c663e210a");
+                var model = LanguageUnderstandingModel.FromAppId(ConfigurationManager.AppSettings.Get("LUISId"));
                 recognizer.AddAllIntents(model);
 
                 // Starts speech recognition, and returns after a single utterance is recognized. The end of a
@@ -42,12 +44,25 @@ namespace SpeechService
                     {
 
                         var luisJson = JObject.Parse(result.Properties.GetProperty(PropertyId.LanguageUnderstandingServiceResponse_JsonResult));
-                        string lng = luisJson["entities"].Where(x => x["type"].ToString() == "Translate.TargetLanguage").First()["entity"].ToString();
-                        string text = luisJson["entities"].Where(x => x["type"].ToString() == "Translate.Text").First()["entity"].ToString();
+                        string targetLng = luisJson["entities"].First(x => x["type"].ToString() == "Translate.TargetLanguage")["entity"].ToString();
+                        string text = luisJson["entities"].First(x => x["type"].ToString() == "Translate.Text")["entity"].ToString();
                         
-                        var lngName = allCultures.FirstOrDefault(c => c.DisplayName.ToLower() == lng.ToLower()).Name;
+                        var lng = allCultures.FirstOrDefault(c => c.DisplayName.ToLower() == targetLng.ToLower()) ??
+                                  allCultures.FirstOrDefault(c => c.DisplayName.ToLower() == "english");
+                        var translated = Translate.TranslateText(lng?.Name, text);
 
-                        Console.WriteLine(Translate.TranslateText(lngName, text));
+                        Console.WriteLine(translated);
+
+                        var synth = new SpeechSynthesizer();
+
+                        // Configure the audio output.   
+                        synth.SetOutputToDefaultAudioDevice();
+
+                        // Speak a string.  
+                        synth.SelectVoice(synth.GetInstalledVoices(lng).First().VoiceInfo.Name);
+                        synth.Speak(translated);
+
+
                     }
                 }
                 else if (result.Reason == ResultReason.RecognizedSpeech)
@@ -74,9 +89,40 @@ namespace SpeechService
             }
         }
 
+        public static async Task TranslationContinuousRecognitionAsync(SpeechTranslationConfig config)
+        {
+            // Sets source and target languages.
+            string fromLanguage = "en-US";
+            config.SpeechRecognitionLanguage = fromLanguage;
+            config.AddTargetLanguage("de");
+
+            // Sets voice name of synthesis output.
+            const string GermanVoice = "de-DE-Hedda";
+            // const string EnglishNeural = "en-US, JessaNeural";
+            config.VoiceName = GermanVoice;
+            // Creates a translation recognizer using microphone as audio input.
+            using (var recognizer = new TranslationRecognizer(config))
+            {
+
+                // Starts continuous recognition. Uses StopContinuousRecognitionAsync() to stop recognition.
+                Console.WriteLine("Say something...");
+                await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+
+                do
+                {
+                    Console.WriteLine("Press Enter to stop");
+                } while (Console.ReadKey().Key != ConsoleKey.Enter);
+
+                // Stops continuous recognition.
+                await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+            }
+        }
+
         static void Main()
         {
-            RecognizeSpeechAsync().Wait();
+            var config = SpeechTranslationConfig.FromSubscription(ConfigurationManager.AppSettings.Get("SpeechKey"), ConfigurationManager.AppSettings.Get("Region"));
+            RecognizeOnceSpeechAsync(config).Wait();
+            //TranslationContinuousRecognitionAsync(config).Wait();
             Console.WriteLine("Please press a key to continue.");
             Console.ReadLine();
         }
