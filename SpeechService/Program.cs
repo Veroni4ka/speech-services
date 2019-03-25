753beba8658f8dad0611824f1f8568add0b55e3e
Veronika <veronika.kolesnikova@yahoo.com>
2019-03-24 23:56:10 -0400

    Add authentication class for text-to-speech

--- SpeechService/Program.cs
+++ SpeechService/Program.cs
@@ -8,6 +8,11 @@ using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Intent;
 using Microsoft.CognitiveServices.Speech.Translation;
 using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Media;
+using System.Xml.Linq;
+using System.Net.Http;
+using System.Text;
 
 namespace SpeechService
 {
@@ -42,7 +47,6 @@ namespace SpeechService
                     Console.WriteLine($"    Language Understanding JSON: {result.Properties.GetProperty(PropertyId.LanguageUnderstandingServiceResponse_JsonResult)}.");
                     if (result.IntentId == "Translate.Translate")
                     {
-
                         var luisJson = JObject.Parse(result.Properties.GetProperty(PropertyId.LanguageUnderstandingServiceResponse_JsonResult));
                         string targetLng = luisJson["entities"].First(x => x["type"].ToString() == "Translate.TargetLanguage")["entity"].ToString();
                         string text = luisJson["entities"].First(x => x["type"].ToString() == "Translate.Text")["entity"].ToString();
@@ -59,10 +63,8 @@ namespace SpeechService
                         synth.SetOutputToDefaultAudioDevice();
 
                         // Speak a string.  
-                        synth.SelectVoice(synth.GetInstalledVoices(lng).First().VoiceInfo.Name);
+                        synth.SelectVoice(synth.GetInstalledVoices().First(x => x.VoiceInfo.Culture.TwoLetterISOLanguageName == lng.TwoLetterISOLanguageName).VoiceInfo.Name);
                         synth.Speak(translated);
-
-
                     }
                 }
                 else if (result.Reason == ResultReason.RecognizedSpeech)
@@ -91,38 +93,80 @@ namespace SpeechService
 
         public static async Task TranslationContinuousRecognitionAsync(SpeechTranslationConfig config)
         {
-            // Sets source and target languages.
-            string fromLanguage = "en-US";
-            config.SpeechRecognitionLanguage = fromLanguage;
-            config.AddTargetLanguage("de");
+            Console.Write("What would you like to convert to speech? ");
+            string text = Console.ReadLine();
+
+            // Gets an access token
+            string accessToken;
+            Console.WriteLine("Attempting token exchange. Please wait...\n");
+
+            // Add your subscription key here
+            // If your resource isn't in WEST US, change the endpoint
+            Authentication auth = new Authentication("https://eastus2.api.cognitive.microsoft.com/sts/v1.0/issuetoken", config.SubscriptionKey);
+            try
+            {
+                accessToken = await auth.FetchTokenAsync().ConfigureAwait(false);
+                Console.WriteLine("Successfully obtained an access token. \n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to obtain an access token.");
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            string host = "https://eastus2.tts.speech.microsoft.com/cognitiveservices/v1";
 
             // Sets voice name of synthesis output.
-            const string GermanVoice = "de-DE-Hedda";
+            const string GermanVoice = "de-DE, Hedda";
             // const string EnglishNeural = "en-US, JessaNeural";
             config.VoiceName = GermanVoice;
             // Creates a translation recognizer using microphone as audio input.
-            using (var recognizer = new TranslationRecognizer(config))
+            XDocument body = new XDocument(
+                    new XElement("speak",
+                        new XAttribute("version", "1.0"),
+                        new XAttribute(XNamespace.Xml + "lang", "de-DE"),
+                        new XElement("voice",
+                            new XAttribute(XNamespace.Xml + "lang", "de-DE"),
+                            new XAttribute("name", "Microsoft Server Speech Text to Speech Voice (" + GermanVoice +")"),
+                            text)));
+
+            using (HttpClient client = new HttpClient())
             {
-
-                // Starts continuous recognition. Uses StopContinuousRecognitionAsync() to stop recognition.
-                Console.WriteLine("Say something...");
-                await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
-
-                do
+                using (HttpRequestMessage request = new HttpRequestMessage())
                 {
-                    Console.WriteLine("Press Enter to stop");
-                } while (Console.ReadKey().Key != ConsoleKey.Enter);
-
-                // Stops continuous recognition.
-                await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+                    // Set the HTTP method
+                    request.Method = HttpMethod.Post;
+                    // Construct the URI
+                    request.RequestUri = new Uri(host);
+                    // Set the content type header
+                    request.Content = new StringContent(body.ToString(), Encoding.UTF8, "application/ssml+xml");
+                    // Set additional header, such as Authorization and User-Agent
+                    request.Headers.Add("Authorization", "Bearer " + accessToken);
+                    request.Headers.Add("Connection", "Keep-Alive");
+                    // Audio output format. See API reference for full list.
+                    request.Headers.Add("X-Microsoft-OutputFormat", "riff-24khz-16bit-mono-pcm");
+                    // Create a request
+                    Console.WriteLine("Calling the TTS service. Please wait... \n");
+                    using (HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        using (Stream dataStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                        {
+                            SoundPlayer snd = new SoundPlayer(dataStream);
+                            snd.Play();
+                        }
+                    }
+                }
             }
         }
 
         static void Main()
         {
             var config = SpeechTranslationConfig.FromSubscription(ConfigurationManager.AppSettings.Get("SpeechKey"), ConfigurationManager.AppSettings.Get("Region"));
-            RecognizeOnceSpeechAsync(config).Wait();
-            //TranslationContinuousRecognitionAsync(config).Wait();
+            //RecognizeOnceSpeechAsync(config).Wait();
+            TranslationContinuousRecognitionAsync(config).Wait();
             Console.WriteLine("Please press a key to continue.");
             Console.ReadLine();
         }
